72b699e878b16af2256c9523bf3c3d175edbf457
Aris Doxakis <adoxakis@akroninc.net>
2015-12-30 12:11:46 +0200

    Initial commit

---
+++ app/src/main/java/gr/dasoftware/notesapp/NavigationDrawerAdapter.java
@@ -0,0 +1,13 @@
+package gr.dasoftware.notesapp;
+
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class NavigationDrawerAdapter extends BaseAdapter {
+    private List<NavigationDrawerItem> mDrawerItems;
+    private LayoutInflater mLayoutInflater;
+
+
+}
